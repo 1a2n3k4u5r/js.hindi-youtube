06af30a9ed349e9c1aeea2286fa05446f7a48b15
1a2n3k4u5r <ankur1236876@gmail.com>
2025-12-27 22:53:27 +0530

    Add files via upload

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,21 @@
+ const accountId = 144553
+ let accountEmail = "ankur1236876@gmail.com"
+ var accountPasswoard = "12345"
+ accountCity = "Jaipur"
+ let accountState;
+
+ // accountId = 2 // not allowed
+
+ accountEmail = "officialboy1236@gmail.com"
+ accountPassword = "21212121"
+ accountCity = "Bengaluru"
+
+ console.log(accountId);
+
+ /*
+ Prefer not to use var
+ because of issue in block scope and functional scope
+ */
+
+ console.table([accountId,accountEmail,accountPasswoard,accountCity,accountState])
+ 
