efbb362687f54b94a2ff9940c27d051de0b90ffa
1a2n3k4u5r <ankur1236876@gmail.com>
2026-01-03 10:14:53 +0530

    Update datatype-comprasion.js

---
+++ 01_basic/datatype-comprasion.js
@@ -0,0 +1,48 @@
+ // primitive
+
+ // 7 types of : Boolean , String, Number, Null, Undefined, Symbol, BigInt
+
+ const score =  100
+ const scorevalue = 100.3
+
+ const isLoggedIn = false
+ const outsideTemp = null
+ let userEmail;
+
+ const id = Symbol ('123');
+ const anotherId = symbol('123');
+
+
+ console.log( id === anotherId);
+
+ const bignumber = 3454356367267n
+
+
+//reference (Non primitive)
+
+//Array, Object , function
+
+const heroes = ["shaktiman" , "naajrag ", "doga"]; // example of array
+
+let myObject =  {
+    name :"Ankur yadav", // example of object
+    age : 20,
+}
+
+const myFunction = function(){
+    console.log("helo World");
+
+}
+
+ consolelog(typeof anotherId);
+ //https://262.ecma-international.org/5.1/#sec-11.4.3
+ 
+
+
+
+
+
+
+
+
+
