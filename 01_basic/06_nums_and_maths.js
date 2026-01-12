22d28c87b77ac014d9de40099516fee1ac1ee231
1a2n3k4u5r <ankur1236876@gmail.com>
2026-01-12 15:19:31 +0530

    Create 06_nums_and_maths.js

---
+++ 01_basic/06_nums_and_maths.js
@@ -0,0 +1,39 @@
+  const score = 400
+  //console.log(score);
+
+  const balance = new Number(100);
+  //console.log(balance);
+
+  //console.log(balance.toString().lenght); //for putting commas between number at the appropriate place.
+  //console.log(balamnce.toFixed(1)); //for putting decimal at a given number.
+
+
+  const otherNumber = 123.8966
+
+  //console.log(otherNumber.toPrecision(4));  //for putting decimal from starting number.
+
+  const hundreds = 1000000
+  //console.log(hundereds.tolocalString('en-IN'));
+
+  //+++++++++++++++ Maths ++++++++++++++++
+  //console.log(Maths);      // object
+  //console.log(Maths.abs(-4));   // convert negative value  in the positive value
+  //console.log(Maths.round(4.6));   
+ // console.log(Maths.ceil(4.2));  // gives maximum value of the decimal number.
+  //console.log(Maths.floor(4.9));  //gives min  value of the  deciaml number .
+  //console.log(Maths.min(4,3,6,8));
+  //console.log(Maths.max(4,3,6,8));
+
+  console.log(Maths.random());
+  console.log((Maths.random()*10) + 1);
+  console.log(Maths.floor(Maths.random()*10) + 1);
+
+  const min = 10;
+  const max = 20;
+
+  console.log(Maths.floor(Maths.random()* (max- min + 1)) + min)
+
+
+
+
+
