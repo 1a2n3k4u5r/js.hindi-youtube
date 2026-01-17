b7fb4233b762ff58f4fb0db63e16c0f88a1e63e8
1a2n3k4u5r <ankur1236876@gmail.com>
2026-01-18 02:03:49 +0530

    Update 04_objects.js

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -54,8 +54,34 @@ console.log(tinderUser); // { id: '123abc', name: 'Sammy', isLoggedIn: false }
 
  console.log(tinderUser.hasOwnProperty('isLoggedIn'))
 
+ 
+ const course = {
+    coursename: "js in hindi",
+    price:"999",
+    courseInstructor: "hitesh"
+ }
+
+ // course.courseInstructor
+
+ const {courseInstructor: instructor} = course
+
+ // console.log(courseInstructor);
+ console.log(instructor);
+
+ // {
+ // "name": "hitesh",
+ // "coursename": "js in hindi",
+ // "price": "free"
+  // }
+
+  [
+    {},
+    {},
+    {}
+  ]
 
 
+ 
 
 
 
