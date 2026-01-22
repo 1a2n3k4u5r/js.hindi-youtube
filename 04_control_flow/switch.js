9d24997909efdf7908643ea9d161f9c900c712f7
1a2n3k4u5r <ankur1236876@gmail.com>
2026-01-22 13:17:14 +0530

    created

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -1,1 +1,29 @@
- 
+//  switch (key){
+//     case Value:
+//         break;
+
+//         default:
+//             break;
+//  }
+  
+
+const month = "march"
+
+ switch (month){
+    case janurary:
+        console.log("January");
+    break;
+     case feburary:
+        console.log("feburary");
+        break;
+     case march:
+        console.log("march");
+        break;
+     case april:
+        console.log("april");
+        break;
+    default:
+        console.log("default case match");
+        break;
+
+ }
