e82d577a942e37078bf96dec1a4a34633870989c
1a2n3k4u5r <ankur1236876@gmail.com>
2026-02-01 12:52:07 +0530

    complete.js

---
+++ 10_classes_and_oop/getter_setter.js
@@ -0,0 +1,27 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+
+
+    set email(value){
+         this._email = value
+    }
+
+    get password(){
+        return `${this._password.toUpperCase()
+    }ankur`
+}
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const hitesh = new User("a@ankur.ai", "123")
+console.log(hitesh.password);
