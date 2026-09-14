8c8b1ede8a07fdd7fa9196d907856a6372af7faa
1a2n3k4u5r <ankur1236876@gmail.com>
2026-09-14 19:39:28 +0530

    revision of conversion

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,30 +1,33 @@
- let score = " Ankur yadav"
+ let score = "33"
 
- console.log (typeof score);
+ console.log (typeof score);   // output are = string
  console.log ((typeof score));
 
-  let valuInNumber = Number(score)
-  //console.log (typeof value InNumber);
- // console.log ( value InNumber);
+  let valuInNumber = Number(score)   // we use this for conversion of string into number
+  //console.log (typeof valueInNumber); output = number.
+ // console.log ( valueInNumber);
 
  // "33" => 33
-  //"33abc" => NaN
+  //"33abc" => NaN, Undefined = NaN
  // true => 1 ;false => 0
 
-  let  isloggedIn = "Ankur yadav"
+  let  isloggedIn = " 1 "
 
-  let booleanIsloggedIn = Boolean(isloggedIn)
-  console.log (booleanIsloggedIn);
+  let booleanIsloggedIn = Boolean(isloggedIn)   // we use this for conversion of number into boolean
+  console.log (booleanIsloggedIn); // output is true
 
  // 1 => true; 0 => false;
-  //"" => false
-// "Ankur yadav" => true;
+ // "" => false
+ // "Ankur yadav" => true;
 
  let someNumber = 33;
 
- let stringNumber = String (someNumber)
+ let stringNumber = String (someNumber)   // // we use this for conversion of number into string
  console.log(stringNumber);
- console.log(typeof stringNumber);
+ console.log(typeof stringNumber);  // output are = string
+
+
+
 
  // ******** Operation ***********//
 
@@ -40,32 +43,32 @@
  //console.log(2/2);
 // console.log(2%2);
 
-let str1 = "hello"
-let str2 = "Ankur yadav"
+ let str1 = "hello"
+ let str2 = " Ankur yadav"
 
-let str3 = str1 + str2
-console.log(str3);
+ let str3 = str1 + str2
+ console.log(str3); // output = hello Ankur yadav
 
-console.log("1" + 2 );
-console.log(1 + "2 ");
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2" );
+ console.log("1" + 2 );  // output = 12
+ console.log(1 + "2 ");  // output = 12
+ console.log("1" + 2 + 2);   // output = 122
+ console.log(1 + 2 + "2" );   //output = 32
 
-console.log( 3 + 4 ) * 5 % 3;
+ console.log( (3 + 4) * 5 % 3 ) ;
 
-console.log(+true);
-console.log(+ "");
+ console.log(+true);   // output = 1
+ console.log(+ "");    // output = 0
 
-let num1 , num2 , num3
+ let num1 , num2 , num3
 
-num1 = num2 = num3 = 2 + 2
+ num1 = num2 = num3 = 2 + 2
 
-let gameCounter = 100
-++gameCounter;
-console.log(gameCounter);
+ let gameCounter = 100
+ ++gameCounter;   // prefix ka andar values phela increment ho jati hai,  postfix ka andar value usage ka bad increment hoti hai.
+ console.log(gameCounter); // output = 101
 
-//link to study
-//https.//tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+ //link to study
+ //https.//tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
 
 
 
