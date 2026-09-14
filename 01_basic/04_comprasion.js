41e575a05539f1574e62684170ba575186e7c028
1a2n3k4u5r <ankur1236876@gmail.com>
2026-09-14 23:18:00 +0530

    Enhance comments and output clarity in 04_comprasion.js

--- 01_basic/04_comprasion.js
+++ 01_basic/04_comprasion.js
@@ -6,19 +6,22 @@
 
 
  console.log("2 > 1");
- console.log("02" > 1);
+ console.log("02" > 1); // output = true
 
 
- console.log(null > 0);
- console.log(null == 0);
- console.log(null >= 0);
+ console.log(null > 0);   // false
+ console.log(null == 0);   // false
+ console.log(null >= 0);    // true.  
+ //  here reason is that an equality check == and comparsions > <>= <= work differently.  Comparisons convert null to a number, treating it as 0. That's why (3) null >= 0 is true and (1) null >0 is false.
+
+ 
 
 
-console.log(undefined == 0);
+console.log(undefined == 0); // false
  console.log(null > 0);
  console.log(null < 0);
 
- //==
+ //=== check the value strictly means uski data type ko bhi check karta hai.
 
  console.log("2" === 2);
  
