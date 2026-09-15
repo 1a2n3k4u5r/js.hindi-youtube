1fe08922b25e4a18641423c84d939abd490043ea
1a2n3k4u5r <ankur1236876@gmail.com>
2026-09-15 20:49:03 +0530

    Refactor console.log statements for clarity and remove commented-out code in 06_nums_and_maths.js and 07_datesinJs.js

--- 01_basic/06_nums_and_maths.js
+++ 01_basic/06_nums_and_maths.js
@@ -1,32 +1,35 @@
   const score = 400
-  //console.log(score);
+  console.log(score);
 
   const balance = new Number(100);
-  //console.log(balance);
+  console.log(balance);  // output = [Number : 100]
 
-  //console.log(balance.toString().lenght); //for putting commas between number at the appropriate place.
-  //console.log(balamnce.toFixed(1)); //for putting decimal at a given number.
+  console.log(balance.toString().length); // output = 3 
+  console.log(balance.toFixed(1)); // output = 100.0    //for putting decimal at a given number.
 
 
   const otherNumber = 123.8966
-
-  //console.log(otherNumber.toPrecision(4));  //for putting decimal from starting number.
+  console.log(otherNumber.toPrecision(4));  // toPrecision() returns a string, not a number and toPrecision() is used to format a number to a specified number of significant digits.
 
   const hundreds = 1000000
-  //console.log(hundereds.tolocalString('en-IN'));
+  console.log(hundreds.toLocaleString('en-IN'));   // toLocalString is used to add a commas(,)
 
-  //+++++++++++++++ Math ++++++++++++++++
-  //console.log(Math);      // object
-  //console.log(Math.abs(-4));   // convert negative value  in the positive value
-  //console.log(Math.round(4.6));   
- // console.log(Math.ceil(4.2));  // gives maximum value of the decimal number.
-  //console.log(Math.floor(4.9));  //gives min  value of the  deciaml number .
-  //console.log(Math.min(4,3,6,8));
-  //console.log(Math.max(4,3,6,8));
 
-  console.log(Math.random());
+
+  //  ********* Math *********
+
+
+  console.log(Math);      // object
+  console.log(Math.abs(-4));   // convert negative value  in the positive value
+  console.log(Math.round(4.6));  //Math.round() decimal number ko nearest full number bana deta hai.
+  console.log(Math.ceil(4.2));  // gives maximum value of the decimal number.
+  console.log(Math.floor(4.9));  //gives min  value of the  deciaml number .
+  console.log(Math.min(4,3,6,8));
+  console.log(Math.max(4,3,6,8));
+
+  console.log(Math.random());   //Math.random() is used to generate a random number between 0 (included) and 1 (not included).
   console.log((Math.random()*10) + 1);
-  console.log(Math.floor(Math.random()*10) + 1);
+  console.log(Math.floor(Math.random()*10) + 1);  // Math.floor() is used to remove the decimal part and give the lower whole number.
 
   const min = 10;
   const max = 20;
@@ -37,3 +40,4 @@
 
 
 
+ 
--- 01_basic/07_datesinJs.js
+++ 01_basic/07_datesinJs.js
@@ -7,19 +7,19 @@
  console.log(typeof myDate);
 
   //let myCreatedDate = new Date (2023, 0, 23) // Mon Jan 23 2023
-  // console.log(myCreatedDate.toDateString());
+   console.log(myCreatedDate.toDateString());
 
-   // let myCreatedDate = new Date(2023, 0, 23, 5, 23)
-   //console.log(myCreatedDate.toLocaleString()); // 1/23/2023, 5:23:00 AM
-   //let myCreatedDate = new Date ("2023-01-14")
+   //let myCreatedDate = new Date(2023, 0, 23, 5, 23)
+   console.log(myCreatedDate.toLocaleString()); // 1/23/2023, 5:23:00 AM
+   // let myCreatedDate = new Date ("2023-01-14")
    let myCreatedDate = new Date ("01-14-2023")
-   // console.log(myCreatedDate.toLocaleString());
+   console.log(myCreatedDate.toLocaleString());
 
    let myTimeStamp = Date.now()
 
-   //console.log(myTimeStamp); // 1768563332049  millisecond value of the date and time
-   //console.log(myCreatedDate.getTime());
-    // console.log(Math.floor(Date.now()/1000));  //1768564604 time in second in minimum value without decimal
+   console.log(myTimeStamp); // 1768563332049  millisecond value of the date and time
+   console.log(myCreatedDate.getTime());
+   console.log(Math.floor(Date.now()/1000));  //1768564604 time in second in minimum value without decimal
 
     let newDate = new Date()
     console.log(newDate);
