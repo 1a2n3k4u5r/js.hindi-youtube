45c86cc7949b3259e15c1d767ae73f0e12618868
1a2n3k4u5r <ankur1236876@gmail.com>
2026-09-16 15:56:01 +0530

    Enhance comments for clarity and detail in 01_functions.js

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -7,24 +7,24 @@
     console.log("R");
  }
 
- // sayMyName()
+ // sayMyName() where sayMyName is a refernce and () is a excution of a function.
  
-  //function addTwoNumbers(number1, number2){  // parameters -> jab bhi aaap function ki definition banata  ha to jo bhi input lata ha number ,string usa parameters bolata ha
+ //function addTwoNumbers(number1, number2){  // parameters -> jab bhi aap function ki definition banata  ha to jo bhi input lata ha number ,string usa parameters bolata ha
   //   console.log(number1 + number2);
  //}
 
- // addTwoNumbers(3, null) // arguments ->  jab aap function ko call karata hai tab uska ander jo value pass hota hai usa  arguments bolta ha
+ // addTwoNumbers(3, 4) // arguments ->  jab aap function ko call karata hai tab uska ander jo value pass hota hai usa  arguments bolta ha
 
  function addTwoNumbers(number1, number2){
 
  // let result = number1 + number2
  // return result
- return number1 + number2
+ return number1 + number2 // by default function k ek definition ka rule hota hai ki ek bar apna jo return kar diya uska bad koi kaam nhi karega function.
  }
 
  const result = addTwoNumbers(3, 5)
 
- // console.log("Result:", result);
+ // console.log("Result:", result);   //output = undefined
 
 
  function loginUserMessage(username){
@@ -36,14 +36,31 @@
  }
 
  //console.log(loginUserMessage("ankur"))
- // console.log(loginUserMessage())
+ // console.log(loginUserMessage()) // output = undefined when value is not passed
 
-function calculateCartPrice(val1, val2, num1){
+
+
+function calculateCartPrice(num1){
     return num1
 }
+ // console.log(calculateCartPrice(200,400,500,2000)) // output = 200
 
- // console.log(calculateCartPrice(200,400,500,2000))
 
+ function calculateCartPrice(...num1){
+    return num1
+}    // here ... is a rest operators
+
+ // console.log(calculateCartPrice(200,400,500,2000)) // output = [200,400,500, 2000]
+
+
+
+ function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}    // here ... is a rest operators
+
+ // console.log(calculateCartPrice(200,400,500,2000)) // output = [500, 2000]
+
+ 
  const user = {
     username:"ankur",
     price:199
@@ -69,3 +86,6 @@ function calculateCartPrice(val1, val2, num1){
 
  //console.log(returnSecondValue(myNewArray));
  console.log(returnSecondValue([200, 400, 500, 1000]));
+
+
+ // jab app koi bhi value return karenga to app usa kisi bhi value variable ma store kar sakta ho.
