9270a360843632efbd20bd341a9a5da44596b556
1a2n3k4u5r <ankur1236876@gmail.com>
2026-09-18 19:55:49 +0530

    Enhance comments for clarity and detail in eight.js

--- 05_iterations.js/eight.js
+++ 05_iterations.js/eight.js
@@ -1,11 +1,15 @@
 const myNums = [1, 2, 3]
 
-// const myTotal = myNums.reduce(function (acc, currval){
+// const myTotal = myNums.reduce(function (acc, currval){   //reduce() is used when you want to convert the  whole array into one single value.       
+
 //     console.log(`acc: ${acc} and currval: ${currval}`);
 //     return acc + currval
-// }, 0)
+// }, 0)    // accumulator ko apko comma ka bad jo bhi value data ho vo valur accumulator ma ati hai .
 
-const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)   // variables are used to store the value
 console.log(myTotal);
 
 const shoppingCart = [
@@ -30,3 +34,16 @@ const shoppingCart = [
  const priceToPay = shoppingCart.reduce( (acc, item) => acc + item.price,0)
 
  console.log(priceToPay)
+
+
+ // Syntax of reduce method - 
+ // array.reduce((accumulator, currentValue) => {
+//     return accumulator + currentValue;
+// }, initialValue);
+
+// accumulator (sum) → stores the result so far
+// currentValue (num) → current array element
+// initialValue → starting value, usually 0 for addition
+
+
+// where we use this (=>) than we called this is a arrow function.
