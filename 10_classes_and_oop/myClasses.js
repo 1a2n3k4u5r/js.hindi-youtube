58d1b64b386061d0e9d21f334889ec221fc29b0c
1a2n3k4u5r <ankur1236876@gmail.com>
2026-09-22 22:56:19 +0530

    Enhance comments for clarity and organization in myClasses.js

--- 10_classes_and_oop/myClasses.js
+++ 10_classes_and_oop/myClasses.js
@@ -1,25 +1,31 @@
 // ES6
+  
+ // ****** Two ways of creating essentially the same kind of User object: ****
 
-class User {    //class is a keyword // constructor is call when an object is initilize from class
+
+      // 1st Way :
+       class User {    //class is a keyword // constructor is call when an object is initilize from class
        constructor(username, email,  password)  {  
         this.username = username;
         this.email = email;
         this.password = password  
      }  
-
-     encryptPassword(){       //methods
+     encryptPassword(){                   //methods
           return  `${this.password}abc`
      }
      changeUsername(){
         return `${this.username.toUpperCase()}`
      }
 }
-
-const chai = new User("chai", "chai@gmail.com", "123")
+const chai = new User("chai", "chai@gmail.com", "123")  // object
 
 console.log(chai.encryptPassword());
 console.log(chai.changeUsername());
 
+
+ 
+
+// 2nd way:
 // behind the scene
 
 function User(username, email, password){
@@ -42,3 +48,21 @@ const tea = new User("tea", "tea@gmail.com", "123")
 
 console.log(tea.encryptPassword());
 console.log(tea.changeUsername());
+
+
+
+// **** Revision ****
+// class is a JavaScript keyword used to create a blueprint/template for objects.
+// Class = blueprint
+ // Object = actual thing created from blueprint
+ // he constructor() is a special method inside a class.
+// It automatically runs when you create an object using new.
+
+// ***** new performs several important steps *****
+// 1. Create a new empty object
+ //        ↓
+// 2. Connect it to User.prototype
+//         ↓
+// 3. Call User/constructor with this = new object
+//         ↓
+// 4. Return the new object
