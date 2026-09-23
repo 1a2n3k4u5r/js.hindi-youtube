25babe8b3c5c67fc0faf680ec5d6ea276eba6ad2
1a2n3k4u5r <ankur1236876@gmail.com>
2026-09-23 08:33:01 +0530

    Enhance comments for clarity and detail in inheritance.js and staticprop.js

--- 10_classes_and_oop/inheritance.js
+++ 10_classes_and_oop/inheritance.js
@@ -1,30 +1,34 @@
-class User {
-    constructor(username){
-    this.username = username
-    }
+      // *****  Parent class — User *****
 
+ class User {
+    constructor(username){ 
+    this.username = username   //username on the right → value received by constructor and this.username on the left → property of the current object
+    }
     logMe(){
-        console.log(`USERNAME is ${this.username}`);
+        console.log(`USERNAME is ${this.username}`);  // ${this.username} = This is template literal syntax.
     }
-}
+ }
 
-  class Teacher extends User{
+
+   // ****** Creating the child class ******
+
+  class Teacher extends User{              // extends = means inheritance = Teacher is a child class of User.
     constructor(username,email,password){
-        super(username)   //Call parent constructor
+        super(username)   // super() = Calls parent call constructor which is User
+
         this.email = email
         this.password = password
     }
-
     addCourse(){
-        // console.log(`A new course was added by ${this.username}`);
+          console.log(`A new course was added by ${this.username}`);
     }
   }
-
-  const chai =  new Teacher("chai", "chai@teacher.com", "123")
+    const chai =  new Teacher("chai", "chai@teacher.com", "123")
 
   chai.addCourse()
+
   const masalaChai = new User("masalaChai")
-
   masalaChai.logMe()
+  console.log(chai instanceof User);  // instanceof checks whether an object belongs to a class's inheritance chain.
 
-  console.log(chai instanceof User);
+  // You cannot use this before super() in a derived class constructor.
--- 10_classes_and_oop/staticprop.js
+++ 10_classes_and_oop/staticprop.js
@@ -7,14 +7,16 @@ class User {
     console.log(`Username: ${this.username}`)
  }
 
-static createId(){     // static is used for access the method and properties
+static createId(){     // static is used for access the method and properties or static means:This method belongs to the class itself, not to the objects created from the class.
     return '123'
     }
  }
 
  const ankur = new User("ankur")
-//   console.log(ankur.createId())
+ console.log(User.createId())
 
+
+ //Creating Teacher
   class Teacher extends User {
     constructor(username,email){
         super(username)
@@ -24,3 +26,5 @@ static createId(){     // static is used for access the method and properties
 
   const iphone = new Teacher("iphone", "i@phone.com")
   iphone.logMe();
+
+  //static is used to define methods/properties that belong to the class itself rather than to individual instances (objects).
