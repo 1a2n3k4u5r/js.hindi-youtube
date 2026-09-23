c622658056178781e4b0a10ee359de27fe2e2f73
1a2n3k4u5r <ankur1236876@gmail.com>
2026-09-23 10:45:11 +0530

    Enhance comments for clarity and detail in bind.html and mathpi.js

--- 10_classes_and_oop/mathpi.js
+++ 10_classes_and_oop/mathpi.js
@@ -1,10 +1,15 @@
-const descripter = Object.getOwnPropertyDescriptor(Math,"PI")
+ const descripter = Object.getOwnPropertyDescriptor(Math,"PI"). // Object.getOwnPropertyDescriptor() = Ye method kisi object ki specific property ke rules/details batata hai. where Math is a Object and PI is a poperty.
 
-console.log(descripter); // output are {  value: 3.141592653589793,  writable: false,   enumerable: false,  configurable: false }
+console.log(descripter); 
+// output are {  value: 3.141592653589793,  
+// writable: false,   writable batata hai ki property ki value change kar sakte ho ya nahi.
+//  enumerable: false,  Ye batata hai ki property ko loops mein enumerate/show kiya jayega ya nahi.
+// configurable: false } Ye batata hai ki property ke descriptor/configuration ko change ya property ko delete kar sakte ho ya nahi.
 
-// console.log(Math.PI);
+console.log(Math.PI);
 // Math.PI = 5
 // console.log(Math.PI);
+
 
 const chai = {
     name: 'ginger chai',
@@ -15,20 +20,32 @@ const chai = {
         console.log("chai nhi bni");
     }
 }
+ console.log(Object.getOwnPropertyDescriptor(chai, "name")); 
+ //  output are {value: 'ginger chai',  writable: true,  enumerable: true,   configurable: true}
+// Iska matlab chai.name normal object property hai aur normally modify/iterate/configure ki ja sakti hai.
 
-// console.log(Object.getOwnPropertyDescriptor(chai, "name")); //  output are {value: 'ginger chai',  writable: true,  enumerable: true,   configurable: true}
 
-// Object.defineProperty(chai, 'name',{
-//     writable: false,
-//     enumerable: false
-// })
 
-// console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+Object.defineProperty(chai, 'name',{  // defineProperty() se hum kisi property ke descriptor rules change kar sakte hain.
+    writable: false,
+    enumerable: false
+ })
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
 
-for(let [key, value] of Object.entries(chai)){
+for(let [key, value] of Object.entries(chai)){   // Object.entries(chai) =  Object ko key-value pairs ki array mein convert karta hai.
     if (typeof value != 'function'){
 
         console.log(`${key} : ${value}`);
     }
     
 }
+
+
+// writable     → change?
+// enumerable   → loop mein show?
+// configurable → property ke rules change/delete?
+// value        → actual value?
+
+
+//  Normally, nahi, JavaScript mein Math.PI ki value change nahi kar sakte.
+// Math.PI ka property descriptor hai or configurable: false hai.
