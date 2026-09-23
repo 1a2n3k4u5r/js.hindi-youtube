31daf9c1de386a2db491d48ca9c62413362414f0
1a2n3k4u5r <ankur1236876@gmail.com>
2026-09-23 11:46:29 +0530

    Enhance comments for clarity and fix formatting in getter_setter.js

--- 10_classes_and_oop/getter_setter.js
+++ 10_classes_and_oop/getter_setter.js
@@ -1,3 +1,5 @@
+
+// New Syntax of getter and setter
 class User {
     constructor(email, password){
         this.email = email;
@@ -14,8 +16,7 @@ class User {
     }
 
     get password(){
-        return `${this._password.toUpperCase()
-    }ankur`
+        return `${this._password.toUpperCase()}ankur`
 }
 
     set password(value){
@@ -23,5 +24,17 @@ class User {
     }
 }
 
-const hitesh = new User("a@ankur.ai", "123")
-console.log(hitesh.password);
+const ankur = new User("a@ankur.ai", "123")
+console.log(ankur.password);
+
+// Getter = property ko read karte waqt function jaisa kaam karta hai.
+// Setter = property ko value assign karte waqt function jaisa kaam karta hai.
+
+// Getter vs Setter
+// Kab chalta hai?= Value read karte waqt ->	Value assign karte waqt
+// Example	= user.email -> user.email = "x"
+// Parameter =	Normally nahi ->	Exactly 1 value
+// return = Usually return karta hai ->	Usually return nahi karta
+// Purpose = Value ko process karke dena ->	Value ko validate/process/store karna
+
+// Aur ek important baat: _email aur _password actual private properties nahi hain. _ sirf convention hai ki “ye internal/stored value hai.”
